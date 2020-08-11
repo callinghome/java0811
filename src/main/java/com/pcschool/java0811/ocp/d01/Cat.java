e6dc1299af72533c05767d41c425b8d0eca9279e
callinghome <c54071089@gs.ncku.edu.tw>
2020-08-11 10:20:58 +0800

    Cat

--- src/main/java/com/pcschool/java0811/ocp/d01/Cat.java
+++ src/main/java/com/pcschool/java0811/ocp/d01/Cat.java
@@ -2,5 +2,9 @@
 package com.pcschool.java0811.ocp.d01;
 
 public class Cat {
-    
+    public String name;
+    public static String type; //品種
+    public int age; //年齡
+    public int price;//價格
+    public int amount; //數量
 }
