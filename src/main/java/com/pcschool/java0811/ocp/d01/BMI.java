45897349dd132c156c9d4ad12aec7921b38447ac
callinghome <c54071089@gs.ncku.edu.tw>
2020-08-11 11:09:38 +0800

    BMI

---
+++ src/main/java/com/pcschool/java0811/ocp/d01/BMI.java
@@ -0,0 +1,14 @@
+
+package com.pcschool.java0811.ocp.d01;
+
+public class BMI {
+    public String name;
+    public double h;
+    public double w;
+    public double bmi;
+    
+    public double calcBMI(){
+        bmi = w / Math.pow(h/100, 2);
+        return bmi;
+    }
+}
